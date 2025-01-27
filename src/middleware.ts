ad770c4d6810ae6afb5160c76a296397d968e7ce
AHMET SARIDAG <ahmet.saridagj@gmail.com>
2025-01-27 18:08:28 +0300

    Redirection bug has been fixed.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,22 @@
-import {
-    clerkMiddleware,
-    createRouteMatcher
-  } from '@clerk/nextjs/server';
-  
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
 const isProtectedRoute = createRouteMatcher([
-    '/dashboard(.*)',
-    '/events(.*)',
-    '/profile(.*)',
-    '/contact(.*)',
+  "/dashboard(.*)",
+  "/events(.*)",
+  "/profile(.*)",
+  "/contact(.*)",
 ]);
-  
+
 export default clerkMiddleware((auth, req) => {
-    if (isProtectedRoute(req)) auth().protect();
+  if (isProtectedRoute(req)) {
+    const { userId } = auth();
+    if (!userId) {
+      return NextResponse.redirect(new URL("/sign-in", req.url));
+    }
+  }
 });
 
-
 export const config = {
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
